6d95d317ecc27290e19d0f540e2acca02366e0a7
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-19 13:20:20 +0200

    ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         /* ćw.4 */
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
-
+        System.out.println("voila");
     }
 }
