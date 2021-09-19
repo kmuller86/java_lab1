81a0aa77471a81484f23e7cd115a70d5d20dafe1
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-19 12:02:44 +0200

    moje pierwsze kroki

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
-
+        System.out.println("Test wiersza z nową linią");
+        System.out.print("Zwykła linia tekstu. ");
+        System.out.print("To jest obok tego, co było wyżej");
+        System.out.printf("\nCzy %s to więcej niż %d ?\n", "jeden", 0);
+        System.out.println("Jest git ;)");
     }
 }
