362b531583267869f7702c0c22974f7b79f51e69
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-19 13:14:33 +0200

    ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         /* ćw.4 */
-
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
     }
