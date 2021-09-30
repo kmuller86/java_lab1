78bc2c45e5442a7c782fca66bfdf46408ac990b3
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-30 10:34:17 +0200

    ćwiczenie 1

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
 //        Użyć każdej z opcji
         System.out.println("Test wiersza z nową linią");
         System.out.print("Zwykła linia tekstu. ");
-        System.out.print("To jest obok tego, co było wyżej");
+        System.out.print("To jest obok tego, co było wyżej.");
         System.out.printf("\nCzy %s to więcej niż %d ?\n", "jeden", 0);
-        System.out.println("Jest git ;)");
-        System.out.printf("%f%n", 5.1473);
+        System.out.println("Java jest spoko!");
+        System.out.printf("%f %n", 5.1473);
         System.out.printf("'%S' %n", "Sławek Majchrzak");
     }
 }
