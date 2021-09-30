bb8cc1ee96b03cb21efad36799dc4ea82aa768f0
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-30 12:44:36 +0200

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -8,35 +8,13 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
-        /* Deklarowanie i inicjalizacja tablic */
-        int[] tab1 = {2, 4, 6, 8, 10};
-        boolean[] tab2 = new boolean[12];
-        String[] tab3 = new String[]{"Alex", "Steve", "John"};
-        char[] tab4;
 
-        /* drógi sposób inicjalizowania tablic */
-        int[] tab5 = new int[5];
-        tab5[0] = 1;
-        tab5[1] = 2;
-        tab5[2] = 3;
-        tab5[3] = 4;
-        tab5[4] = 5;
-        System.out.println(tab5.length); // 5
-
-        /* wyświetlanie elementów tablicy */
-        System.out.println("tab1: " + tab1); // [I@36baf30c
-        System.out.println("tab1[0]: " + tab1[0]); // tab1[0]: 2
-        System.out.println("tab3[2]: " + tab3[2]); // tab5[1]: John
-
-        /* przykład przypisania wartości w pętli for */
-        int[] tab6 = new int[5];
-        for (int i = 0; i < tab6.length; i++) {
-            tab6[i] = i + 5;
+        double[] tab1 = new double[] {1.1, 2.2, 3.3, 4.4, 5.5};
+        for (int i = 0; i < tab1.length; i++) {
+            System.out.print(tab1[i] + " ");
         }
-
-        /* przykład wyświetlania wartości w pętli for */
-        for (int i = 0; i < tab6.length; i++) {
-            System.out.print(tab6[i] + " "); // 5 6 7 8 9
-        }
+        // najgorsze co może być ;).
+        System.out.println("\n" + tab1[0]+ " " + tab1[1]+ " " +tab1[2]+ " " +tab1[3]+ " " +tab1[4]);
+        System.out.print("Tablica posiada " + tab1.length + " elementów/ty.");
     }
 }
