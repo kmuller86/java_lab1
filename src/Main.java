d0651e80cb5a107ae3ca0885956157c5b4143cd8
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-30 13:00:08 +0200

    ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         /* Tablice - cw_6 */
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
