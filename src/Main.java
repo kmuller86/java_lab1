47a0cdd733755960c44eaf3807904701364b404e
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-09-30 12:31:43 +0200

    ćwiczenie 5

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,51 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int zmienna = 15;
-        /* pojedynczy if, kod z wewnątrz if-a jest wykonywany tylko gdy warunek jest prawdziwy,
-         * w innym wypadku kod jest pomijany przez program */
-        if (zmienna <= 10) {
-            System.out.println("1) zmienna jest mniejsza od 10");
+        /* ćw.5 */
+
+        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
+         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
+         * */
+        int wiek = 48;
+        int modulo = 3;
+        int wynik;
+        if (wiek % modulo == 0) {
+            wynik = 0;
+            System.out.println("Wynik modulo: " + wynik + " Podzielny przez: " + modulo);
+        } else {
+            wynik = (wiek % modulo);
+            System.out.println("Wynik modulo: " + wynik + " Niepodzielny prze: " + modulo);
         }
 
-        /* konstrukcja else jest wykonywana tylko wtedy gdy wartość w if-ie jest nieprawdziwa,
-         * w innym wypadku kod z else-a jest pomijany */
-        if (zmienna > 10) {
-            System.out.println("2) zmienna jest wieksza od 10");
+        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
+        int mojIndex = 60473;
+        String czyParzysta;
+        if (mojIndex % 2 == 0) {
+            czyParzysta = "parzysta";
         } else {
-            System.out.println("2) zmienna jest <= 10");
+            czyParzysta = "nieparzysta";
         }
+        System.out.println(
+                "Liczba " + mojIndex + " jest " + czyParzysta);
 
-        /* parametr trójargumentowy działa jak if else ale zapis w niektórych przypadkach
-         * jest bardziej estetyczny lub czytelny */
-        int wynik = (zmienna % 2 == 0) ? 0 : 1;
-        System.out.println("3) wynik: " + wynik);
+        // albo raczej to:
+        int numerindeksu = 60473;
+        String parzysta = (numerindeksu % 2 == 0) ? "parzysta" : "nieparzysta";
+        System.out.println("Ten indeks to liczba " + parzysta + ".");
 
-        /* Konstrukcja else if */
-        if (zmienna < 5) {
-            System.out.println("4) zmienna < 5");
-        } else if (zmienna > 15) {
-            System.out.println("4) zmienna > 15");
-        } else if (zmienna == 10) {
-            System.out.println("4) zmienna == 10");
-        } else {
-            System.out.println("5) wykonał sie else, zmienna > 5 i < 15 i !10");
+        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
+         * i stwórz konstrukcję else if z dowolnymi komunikatami */
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Wpisz liczbę: ");
+        double num = sc.nextDouble();
+        if(num > 0)
+        {
+            System.out.println(num+" jest liczbą dodatnią.");
+        }
+        else
+        {
+            System.out.println(num+" jest liczbą ujemną.");
         }
     }
 }
